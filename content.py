54cc06c764e504abc30ff6eabc013674be089a66
Aland Mulia Pratama <13522124@std.stei.itb.ac.id>
2024-05-15 21:16:24 +0700

    feat: integrate entry card to information

--- content.py
+++ content.py
@@ -1,12 +1,13 @@
 class Content:
-    def __init__(self, id, name, duration, summary, genre, rating, lastPLay, gambar):
+    def __init__(self, id, name, releaseYear, duration, summary, genre, rating, watchProgress, gambar):
         self.id = id
         self.name = name
+        self.releaseYear = releaseYear
         self.duration = duration
         self.summary = summary
         self.genre = genre
         self.rating = rating
-        self.lastPLay = lastPLay
+        self.watchProgress = watchProgress
         self.gambar = gambar
     
     def getId(self):
@@ -14,6 +15,9 @@ class Content:
     
     def getName(self):
         return self.name
+    
+    def getReleaseYear(self):
+        return self.releaseYear
     
     def getDuration(self):
         return self.duration
@@ -27,8 +31,8 @@ class Content:
     def getRating(self):
         return self.rating
     
-    def getLastPlay(self):
-        return self.lastPLay
+    def getWatchProgress(self):
+        return self.watchProgress
     
     def getGambar(self):
         return self.gambar
@@ -38,6 +42,12 @@ class Content:
 
     def setName(self, name):
         self.name = name
+
+    def setReleaseYear(self, releaseYear):
+        self.releaseYear = releaseYear
+    
+    def setLastplay(self, lastplay):
+        self.lastplay = lastplay
 
     def setDuration(self, duration):
         self.duration = duration
@@ -51,30 +61,44 @@ class Content:
     def setRating(self, rating):
         self.rating = rating
     
-    def setLastPlay(self, lastPLay):
-        self.lastPLay = lastPLay
+    def setWatchProgress(self, watchProgress):
+        self.watchProgress = watchProgress
     
     def setGambar(self, gambar):
         self.gambar = gambar
 
 class Movie(Content):
-    def __init__(self, id, name, duration, summary, genre, rating, lastPLay, gambar):
-        super().__init__(id, name, duration, summary, genre, rating, lastPLay, gambar)
+    def __init__(self, id, name, releaseYear, duration, summary, genre, rating, watchProgress, gambar):
+        super().__init__(id, name, releaseYear, duration, summary, genre, rating, watchProgress, gambar)
 
 class Series(Content):
-    def __init__(self, id, name, duration, summary, genre, rating, lastPLay, gambar, season, episode):
-        super().__init__(id, name, duration, summary, genre, rating, lastPLay, gambar)
+    def __init__(self, id, name, releaseYear, duration, summary, genre, rating, watchProgress, gambar, season, episode, seasonProgress, episodeProgress):
+        super().__init__(id, name, releaseYear, duration, summary, genre, rating, watchProgress, gambar)
         self.season = season
+        self.seasonProgress = seasonProgress
         self.episode = episode
+        self.episodeProgress = episodeProgress
     
     def getSeason(self):
         return self.season
     
+    def getSeasonProgress(self):
+        return self.seasonProgress
+    
     def getEpisode(self):
         return self.episode
+    
+    def getEpisodeProgress(self):
+        return self.episodeProgress
     
     def setSeason(self, season):
         self.season = season
     
+    def setSeasonProgress(self, seasonProgress):
+        self.seasonProgress = seasonProgress
+    
     def setEpisode(self, episode):
         self.episode = episode
+
+    def setEpisodeProgress(self, episodeProgress):
+        self.episodeProgress = episodeProgress
--- film_information.py
+++ film_information.py
@@ -1,98 +1,122 @@
 import flet as ft
 from content import Content, Movie, Series
 
-class FilmInformation:
+import flet as ft
+
+class FilmInformation(ft.Container):
     def __init__(self, content: Content, page: ft.Page):
-        self.name = ft.Container(
-            ft.Text(content.getName())
+        super().__init__()
+        self.width = page.window_width
+        self.height = page.window_height
+        
+        name = ft.Container(
+            padding=ft.padding.only(left = 20),
+            content = ft.Text(content.getName(), weight=ft.FontWeight.BOLD, size=25, color="#CBA133"))
+        summary = ft.Container(
+            padding=ft.padding.only(left = 20),
+            content = ft.Text(content.getSummary(), weight=ft.FontWeight.W_700, size=15, color="#FED466",)
         )
-        
-        self.summary = ft.Container(
-            ft.Text(content.getSummary())
+        genre = ft.Container(
+            padding=ft.padding.only(left = 20),
+            content = ft.Row(
+                [
+                    ft.Text("Genre : ", color="#FED466", weight=ft.FontWeight.W_700, size=15),
+                    ft.Text(content.getGenre(), color="#FED466", weight=ft.FontWeight.W_700, size=15)
+                ]
+            )
         )
-        
-        self.genre = ft.Container(
-            ft.Text(content.getGenre())
+        releaseYear = ft.Container(
+            padding=ft.padding.only(left = 20),
+            content = ft.Row(
+                [
+                    ft.Text("Release Year : ", color="#FED466", weight=ft.FontWeight.W_700, size=15),
+                    ft.Text(content.getReleaseYear(), color="#FED466", weight=ft.FontWeight.W_700, size=15)
+                ]
+            )
         )
-        
-        # self.releaseDate = ft.Container(
-        #     ft.Text(content.getReleaseDate())
-        # )
-        
-        self.rating = ft.Container(
-            ft.Text(content.getRating())
+        rating = ft.Container(
+            padding=ft.padding.only(left = 20),
+            content =
+            ft.Text(f"Rating : {content.getRating()}/10", color="#FED466", weight=ft.FontWeight.W_700, size=15)
         )
-        
-        self.duration = ft.Container(
-            ft.Text(content.getDuration())
+        duration = ft.Container(
+            padding = ft.padding.only(left = 20),
+
+            content = ft.Text(f"Duration : {content.getDuration()// 3600} hours {(content.getDuration() % 3600) // 60} minutes {content.getDuration() % 60} seconds", color="#FED466", weight=ft.FontWeight.W_700, size=15)
         )
-        
-        self.lastPlay = ft.Container(
-            ft.Text(content.getLastPlay())
+        watchProgress = ft.Container(
+            padding=ft.padding.only(left = 20),
+            content = ft.Text(f"Watch Progress : {content.getWatchProgress()// 3600} hours {(content.getWatchProgress() % 3600) // 60} minutes {content.getWatchProgress() % 60} seconds", color="#FED466", weight=ft.FontWeight.W_700, size=15)
         )
+        gambar = ft.Container(ft.Image(content.getGambar(), width=300, height=450, border_radius=30))
         
-        self.gambar = ft.Container(
-            ft.Image(content.getGambar(), width=300, height=450, border_radius=30),
-        )
-        
-        self.header = ft.Container(
-            content = ft.Row([
-                ft.IconButton(ft.icons.ARROW_BACK, icon_color="#FED466"),
-                ft.Text("Film Information", size=20),
+        header = ft.Container(
+            content=ft.Row([
+                ft.IconButton(ft.icons.ARROW_BACK, icon_color="#FED466", on_click=self.go_back),
+                ft.Text("Film Information", size=30, weight=ft.FontWeight.W_800),
                 ft.Text("")
             ], alignment=ft.MainAxisAlignment.SPACE_BETWEEN)
         )
         
-        self.data = ft.Container(
+        edit = ft.Container(
+            padding=ft.padding.only(right=25),
+            content=ft.Row([
+                ft.ElevatedButton("Edit Information",  bgcolor='#DAAB2D', color='#000000')
+            ], alignment=ft.MainAxisAlignment.END)
+        )
+
+        data = ft.Container(
             content=ft.Column([
-                self.name,
-                self.summary,
-                self.genre,
-                self.rating,
-                self.duration,
-                self.lastPlay,
-                ft.ElevatedButton("Edit Information")
-            ], alignment=ft.MainAxisAlignment.SPACE_EVENLY),
+                name,
+                summary,
+                genre,
+                releaseYear,
+                rating,
+                duration,
+                watchProgress,
+                edit
+            ], 
+            alignment=ft.MainAxisAlignment.SPACE_AROUND),
             bgcolor="#092143",
             margin=ft.margin.only(left=20),
             height=450,
-            width= page.window_width - 500
+            width=page.window_width - 500,
+            border_radius=30
         )
         
-        self.content = ft.Container(
-            content = ft.Row([
-                self.gambar,
-                self.data
+        content_container = ft.Container(
+            content=ft.Row([
+                gambar,
+                data
             ])
         )
         
-        self.delete = ft.Container(
+        delete = ft.Container(
+            padding=ft.padding.only(right=50),
             content=ft.Row([
-                ft.ElevatedButton("Delete Film")
+                ft.ElevatedButton("Delete Film", bgcolor=ft.colors.RED, color = '#ffffff')
             ], alignment=ft.MainAxisAlignment.END)
         )
-    
-    def show_page(self, page: ft.Page):
-        page.add(ft.Container(
-            content=ft.Column([
-                self.header,
-                self.content,
-                self.delete
-                ]),
-            width = page.window_width,
-            height = page.window_height,
-            bgcolor="#000D20"
-        ))
         
-series = Movie("1", "The Falcon and The Winter Soldier", 50, "Sam Wilson and Bucky Barnes realize that their futures are anything but normal.", ["Action", "Adventure", "Drama"], 8.0, "2021-04-23", "https://www.themoviedb.org/t/p/original/6kbAMLteGO8yyewYau6bJ683sw7.jpg")
+        self.content = ft.Column([
+            header,
+            content_container,
+            delete
+        ])
+        self.page = page
 
-def main(Page: ft.Page):
-    Page.window_height = 720
-    Page.window_width = 1280
+    def go_back(self, e):
+        self.page.go("/")
+
+# series = Movie("1", "The Falcon and The Winter Soldier", 50, "Sam Wilson and Bucky Barnes realize that their futures are anything but normal.", ["Action", "Adventure", "Drama"], 8.0, "2021-04-23", "https://www.themoviedb.org/t/p/original/6kbAMLteGO8yyewYau6bJ683sw7.jpg")
+
+# def main(Page: ft.Page):
+#     Page.window_height = 720
+#     Page.window_width = 1280
     
-    film = FilmInformation(series, Page)
+#     film = FilmInformation(series, Page)
     
-    film.show_page(Page)
-    ...
+#     film.show_page(Page)
+#     ...
     
-ft.app(target=main)
+# ft.app(target=main)
